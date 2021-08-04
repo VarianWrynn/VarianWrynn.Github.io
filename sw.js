5341156ec0c99b899bbb2a40338fd78b52372594
Lee.Wang <yuna1111@163.com>
2021-08-04 13:10:47 +0800

    2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210803.1803';
+const cacheName = 'chirpy-20210804.1307';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
